d761bcf09a7758b56597f69a7893da9c36b5f907
mehmehmehlol <hkmeganlo@gmail.com>
2020-08-27 00:25:54 -0700

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,9 +2,15 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand)
+    # BRANDS.uniq
   end
+
+  
 
   def cobble
     self.condition = "new"
